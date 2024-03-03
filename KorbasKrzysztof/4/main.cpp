1730edf9ce3f1e74c6a80817b4a488ae36afb420
HalmonCelman <halmoncelman@gmail.com>
2024-03-03 14:38:07 +0100

    4 not checked

---
+++ KorbasKrzysztof/4/main.cpp
@@ -0,0 +1,20 @@
+#include "led.h"
+#include "stepper.h"
+
+void Delay(int iTimeInMs){
+	int iCycle;
+	int iNumberOfCycles = 10000 * iTimeInMs;
+	
+	for (iCycle = 0; iCycle < iNumberOfCycles; iCycle++) {}
+}
+
+Led My_Led;
+Stepper My_Stepper;
+
+int main(void)
+{
+	while(1){
+		Delay(100);
+		My_Stepper.StepLeft();
+	}
+}
